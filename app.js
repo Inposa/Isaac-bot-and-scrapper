8afa6fdb69edbb4d4aa60305df999ff4706f1771
Inposa <donhippo04@gmail.com>
2021-08-07 01:29:03 +0200

    set-up a little more

---
+++ app.js
@@ -0,0 +1,54 @@
+const fs = require("fs");
+const Discord = require("discord.js");
+const client = new Discord.client();
+
+const localization = require("./localization.json"); 
+const language = "fr";
+
+client.commands = new Discord.Collection();
+
+//Préfix pour appeler le bot
+const prefix = "test!"; // TODO : à changer avec dotenv et sans doute enmap
+
+//lecture et mise en cache des commandes
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
+
+// association nom -> commande pour chaque commande mise en cache
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.endsWith(command.name, command);
+}
+
+client.on("ready", () => {
+  console.log("Démarage du bot Inpobot v2");
+  console.log()
+  return;
+});
+
+client.on("error", (error) => {
+  console.error("Une erreur est survenue. \n", error);
+  return;
+});
+
+
+client.on("message", (message)=>{
+  if(message.author.bot || !message.content.startWith(prefix.toLowerCase())){
+    return;
+  }
+
+  const args = message.content.slice(prefix.length).split(/ +/);
+  const commandName = args.shift().toLowerCase;
+  // console.log(blablabla)
+  if(!client.commands.has(commandName)){
+    return;
+  }
+  
+  const command = client.commands.get(commandName);
+
+  if(command.guildOnly && !message.guild){
+    message.channel.send();
+  }
+  //TODO 
+})
